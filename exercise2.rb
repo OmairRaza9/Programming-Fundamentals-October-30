c1713b10e4f08e1a210fbbe0e879596e5c59befe
Omair <omairraza9@gmail.com>
2017-10-30 13:37:09 -0400

    completed ex2 and started ex1

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+#How would you calculate a good tip for a 55
+# dollar meal? Use puts to print the answer.
+# I used string interpolation to for the base price of 55 * 15% {55*.15}
+puts "1) A good tip for a $55 meal is 15% of the menu price. So it would be $55 (cost of meal) x 15% = Tip (#{55*0.15})"
+
+
+#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer
+# into a string first and use puts to print the result.
+
+NOT SURE about this one
+first = "Omair"
+last  = "Raza"
+first + last
+puts "2) My name is #{first} #{last}"
+#Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+#puts #{45628*7839}
+puts "3) The result of multiplying 45628 x 7839 is #{45628*7839}"
+
+
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+#fale || true
+#true
+puts "4) what is the result of (10 < 20 && 30 < 20) || !(10 == 11) /n The answer: /n #{(10 < 20 && 30 < 20) || !(10==11)}"
